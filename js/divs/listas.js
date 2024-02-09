33effd2e071ecbbf811f9171177ba8acde78b01f
Eber L C Elias <71910158+ebertlc@users.noreply.github.com>
2024-02-09 16:29:36 -0300

    Update listas.js

--- js/divs/listas.js
+++ js/divs/listas.js
@@ -7,29 +7,30 @@ const funcoes_cargo_2 = []
 
 empresa.push(
     'Ministério do Desenvolvimento Regional (MIDR) | 2023 - Atual',
+    'Ministério do Desenvolvimento Regional (MIDR) | 2023 - 2023',
     'UCB - Universidade Católica de Brasília | 2021 - 2022',
-    'UCB - Universidade Católica de Brasília | 2021 - 2022',
-    'BS - Business Services | 2019 - 2021',
 )
 cargo.push(
+    'Auxiliar',
     'Estagiário TI',
-    'Auxiliar Administrativo / Tutoria EAD',
-    'Operador de Telemarketing',
+    'Tutor EAD',
 )
 
 funcoes_cargo_0.push(
+    'Auxiliar e geremciar nos processos relacionados ao desenvolvimento de sistemas relativos a SEDEC;',
+    'Apoiar, Extrair e Analisar dados de interesse da secretaria;',
+    'Apoiar na elaboração de relatórios e apresentações de dados;',
+    'Manter e executar rotinas de backup e recuperação de dados;',
+    'Criar e executar scripts de automação de tarefas e atualizações em banco de dados ORACLE e Postgres;',
+    'Criar e manter documentação técnica dos sistemas e processos da SEDEC.'
+)
+funcoes_cargo_1.push(
     'Auxiliar nos processos relacionados ao desenvolvimento de sistemas relativos a SEDEC;',
     'Apoiar, Extrair e Analisar dados de interesse da secretaria, com acompanhamento direto do responsável técnico.'
 )
-funcoes_cargo_1.push(
+funcoes_cargo_2.push(
     'Acompanhar e motivar a performance e a frequência dos alunos no Ambiente Virtual de Aprendizagem;',
     'Apoiar os discentes no acesso às suas atividades;',
     'Realizar atendimento e esclarecimento de dúvidas técnicas via email, presencial ou por telefone;',
     'Manter e alimentar as planilhas, relatórios, dashboards e outros documentos solicitados pela gestão.'
-)
-funcoes_cargo_2.push(
-    'Atendimento ativo e receptivo via Telefone ou chat;',
-    'Atualização e cadastramento de clientes via preenchimento de dados;',
-    'Condução da solução de tratativas online, dirimindo dúvidas ou encaminhando para o superior;',
-    'Elaboração de relatórios de atendimento e/ou resolução das tratativas online.'
 )
